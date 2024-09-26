a484128102e3e4456b092db275c6ba3d588ecf54
wanmaimarkz <marwan115613@gmail.com>
2024-09-26 21:21:34 +0700

    report-form-ui
    frontend ui

--- activitybookingapp/booking/forms.py
+++ activitybookingapp/booking/forms.py
@@ -1,7 +1,16 @@
 from django.forms import ModelForm
-from booking.models import *
+from booking.models import Report
 
 class ReportForm(ModelForm):
     class Meta:
         model = Report
         fields = "__all__"
+    
+    def __init__(self, *args, **kwargs):
+        super(ReportForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight font-normal'
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        return cleaned_data
--- activitybookingapp/booking/views.py
+++ activitybookingapp/booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from booking.forms import ReportForm
 from booking.models import *
@@ -17,9 +17,20 @@ class ActivityBooking(View):
         return render(request, 'calender.html')
 
 class ReportView(View):
-
     def get(self, request):
         form = ReportForm()
         return render(request, 'report-form.html',{
             "form": form
+        })
+    def post(self, request):
+        form = ReportForm(request.POST)
+
+        if form.is_valid():
+            student = form.cleaned_data.get('student')
+            print(f"Student: {student}")
+            form.save()
+            return redirect('report-form')
+        
+        return render(request, 'report-form.html', {
+            "form":form
         })
