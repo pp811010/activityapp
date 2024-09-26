83c4a5f1e75e0201e039f04dadd000a90abae20d
pp811010 <pp811010@gmail.com>
2024-09-26 16:33:56 +0700

    update

--- activitybookingapp/booking/urls.py
+++ activitybookingapp/booking/urls.py
@@ -5,4 +5,5 @@ from booking import views
 urlpatterns = [
     path("mybooking/", views.MyBooking.as_view(), name="mybooking"),
     path("activity/", views.Activity.as_view(), name="activity"),
+    path("activitybooking/", views.ActivityBooking.as_view(), name="activity"),
 ]
--- activitybookingapp/booking/views.py
+++ activitybookingapp/booking/views.py
@@ -10,3 +10,8 @@ class MyBooking(View):
 class Activity(View):
     def get(self, request):
         return render(request, 'activity.html')
+    
+class ActivityBooking(View):
+    def get(self, request):
+        return render(request, 'calender.html')
+
