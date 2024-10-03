47f45894ffe90995d1d1de76ff2b090541828b57
pp811010 <pp811010@gmail.com>
2024-10-03 17:58:56 +0700

    chang bg

--- activitybookingapp/booking/models.py
+++ activitybookingapp/booking/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Staff(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=150)  # Changed to EmailField for better validation
+    email = models.EmailField(max_length=150) 
     phone = models.CharField(max_length=100)
 
     def __str__(self):
